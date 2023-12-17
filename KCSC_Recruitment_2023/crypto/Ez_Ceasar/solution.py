41fb020c14e4da0e18e2e7991b8623e081e48806
vanatka10 <126310360+vanatka10@users.noreply.github.com>
2023-12-17 22:16:18 +0700

    Update solution.py

--- KCSC_Recruitment_2023/crypto/Ez_Ceasar/solution.py
+++ KCSC_Recruitment_2023/crypto/Ez_Ceasar/solution.py
@@ -1,27 +1,19 @@
 import string
 
 alphabet = string.ascii_letters + string.digits + "!{_}?"
-ct = 'ldtdMdEQ8F7NC8Nd1F88CSF1NF3TNdBB1O'  # Replace with your actual ciphertext
 
-def decrypt_caesar(ciphertext, key):
-    decrypted_text = ""
-    for i in ciphertext:
-        decrypted_text += (alphabet[(alphabet.index(i) - key) % len(alphabet)])
-    return decrypted_text
+def decrypt(cipher, key):
+    flag = ""
+    for i in cipher:
+        flag += alphabet[(alphabet.index(i) + key) % len(alphabet)]
+        print(flag)
+    return flag
 
-def find_key(ciphertext, prefix, alphabet):
-    for key in range(len(alphabet)):
-        decrypted_text = decrypt_caesar(ciphertext, key)
-        if decrypted_text.startswith(prefix):
-            return key
-    return None
+def find_key(cipher):
+    for key in range(0, 2**256):
+        print(key)
+        if decrypt(cipher, key).startswith('KCSC{'):
+            print(key, decrypt(cipher, key))
+            break
 
-prefix = 'KCSC{'
-found_key = find_key(ct, prefix, alphabet)
-
-if found_key is not None:
-    print(f"Found key: {found_key}")
-    decrypted_flag = decrypt_caesar(ct, found_key)
-    print(f"Decrypted flag: {decrypted_flag}")
-else:
-    print("Key not found.")
+find_key('ldtdMdEQ8F7NC8Nd1F88CSF1NF3TNdBB1O')
